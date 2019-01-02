57427d1c963159746cd417650e3032dae4ed5973
rizki-agyadata <rizki@sddn.co.id>
2019-01-02 16:29:51 +0700

    configurasi .vuepress dan guide

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: 'Epigoni',
+    description: 'Epigoni Documentation Site',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' }
+      ],
+      sidebar: [
+        '/',
+        '/guide/'
+      ]        
+    }
+    
+  }
